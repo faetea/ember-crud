8fc51cff83fd318d3011925324e13b37ca78f00c
saphron <saphron.scul@gmail.com>
2015-12-09 13:58:38 -0500

    Added Delete

--- app/components/pokemon-snippet/component.js
+++ app/components/pokemon-snippet/component.js
@@ -9,6 +9,10 @@ export default Ember.Component.extend({
     updatePokemon: function(){
       console.log('Component Action : updatePokemon');
       this.sendAction('routeUpdatePokemon');
+    },
+    destroyPokemon: function(){
+      console.log('Component Action : destroyPokemon');
+      this.sendAction('routeDestroyPokemon');
     }
-  }
+  } // end actions
 });

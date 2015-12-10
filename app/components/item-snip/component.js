7987e71f5854c465fae09fe59b977ff78006a9d1
saphron <saphron.scul@gmail.com>
2015-12-10 00:23:12 -0500

    items fixed item.id to display Added more items to fixture

--- app/components/item-snip/component.js
+++ app/components/item-snip/component.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'li',
   isExpanded: false,
   actions: {
     toggleExpanded: function() {
--- app/mirage/fixtures/items.js
+++ app/mirage/fixtures/items.js
@@ -1,32 +1,74 @@
 export default [
   {
     id: 1,
-    name: 'Ability Capsule',
-    category: 'Hold items',
-    effect: 'A capsule that allows a Pokémon with two Abilities to switch between these Abilities when it is used.'
-  },
-  {
-    id: 2,
     name: 'Ability Urge',
     category: 'Battle items',
     effect: 'When used, it activates the Ability of an ally Pokémon.'
   },
   {
+    id: 2,
+    name: 'Dire Hit',
+    category: 'Battle items',
+    effect: 'Raises critical-hit ratio of a Pokémon in battle.'
+  },
+  {
     id: 3,
-    name: 'Abomasite',
-    category: 'Hold items',
-    effect: 'Enables Abomasnow to Mega Evolve during battle.'
+    name: 'Aspear Berry',
+    category: 'Berries',
+    effect: 'If held by a Pokémon, it defrosts it.'
   },
   {
     id: 4,
-    name: 'Absolite',
-    category: 'Hold items',
-    effect: 'Enables Absol to Mega Evolve during battle.'
+    name: 'Babiri Berry',
+    category: 'Berries',
+    effect: 'Weakens a supereffective Steel-type attack against the holding Pokémon.'
   },
   {
     id: 5,
-    name: 'Absorb Bulb',
+    name: 'Armor Fossil',
+    category: 'General items',
+    effect: 'A fossil from a prehistoric Pokémon that lived on the land. It appears to be part of a collar.'
+  },
+  {
+    id: 6,
+    name: 'BalmMushroom',
+    category: 'General items',
+    effect: 'A rare mushroom which gives off a nice fragrance. A maniac will buy it for a high price.'
+  },
+  {
+    id: 7,
+    name: 'Amulet Coin',
     category: 'Hold items',
-    effect: 'A consumable bulb. If the holder is hit by a Water-type move, its Sp. Atk will rise.'
-  }
+    effect: 'An item to be held by a Pokémon. It doubles a battle’s prize money if the holding Pokémon joins in.'
+  },
+  {
+    id: 8,
+    name: 'Banettite',
+    category: 'Hold items',
+    effect: 'Enables Banette to Mega Evolve during battle.'
+  },
+  {
+    id: 9,
+    name: 'Awakening',
+    category: 'Medicine',
+    effect: 'A spray-type medicine. It awakens a Pokémon from the clutches of sleep.'
+  },
+  {
+    id: 10,
+    name: 'Berry Juice',
+    category: 'Medicine',
+    effect: 'A 100% pure juice made of Berries. It restores the HP of one Pokémon by just 20 points.'
+  },
+  {
+    id: 11,
+    name: 'Cherish Ball',
+    category: 'Pokeballs',
+    effect: 'A quite rare Poké Ball that has been specially crafted to commemorate an occasion of some sort.'
+  },
+  {
+    id: 12,
+    name: 'Dive Ball',
+    category: 'Pokeballs',
+    effect: 'A somewhat different Poké Ball that works especially well on Pokémon that live underwater.'
+  },
 ];

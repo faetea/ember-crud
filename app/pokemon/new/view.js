cf07c6879d8144e82fe5d57a5234270fb30c5d18
saphron <saphron.scul@gmail.com>
2015-12-11 21:09:28 -0500

    added styling to navbar, footer, index card, buttons, and new pokemon form

--- app/pokemon/new/view.js
+++ app/pokemon/new/view.js
@@ -4,6 +4,10 @@ export default Ember.View.extend({
   newPokemon: {
     nationalPokeNum: null,
     name: null,
+    species: null,
+    height: null,
+    weight: null,
+    abilities: null,
     typeOne: null,
     typeTwo: null,
     generation: null

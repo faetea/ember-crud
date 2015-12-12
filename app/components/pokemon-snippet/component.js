5f89dbbc58b34877734268a454a3fb15e2ceb81b
saphron <saphron.scul@gmail.com>
2015-12-11 23:22:24 -0500

    styling component to accordian

--- app/components/pokemon-snippet/component.js
+++ app/components/pokemon-snippet/component.js
@@ -1,24 +1,44 @@
 import Ember from 'ember';
+const { computed, Component } = Ember;
 
 export default Ember.Component.extend({
+  tagName: 'ul',
+  classNames: ['collapsible'],
+  attributeBindings: ['data-collapsible'],
+  accordion: true,
+
+  'data-collapsible': computed(function() {
+    return this.get('accordion') ? 'accordion' : 'expandable';
+  }),
+
+  didInsertElement() {
+    this._super(...arguments);
+    this._setupCollapsible();
+  },
+
+  _setupCollapsible() {
+    const accordion = this.get('accordion');
+    this.$().collapsible({ accordion });
+  },
+
+  _teardownCollapsible() {
+    const $panelHeaders = this.$('> li > .collapsible-header');
+    this.$().off('click.collapse', '.collapsible-header');
+    $panelHeaders.off('click.collapse');
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    this._teardownCollapsible();
+  },
+
   twoTypes: Ember.computed('pokemon.typeOne', 'pokemon.typeTwo', function(){
     return this.get('pokemon.typeTwo') && this.get('pokemon.typeTwo') !== this.get('pokemon.typeOne');
   }),
   twoAbilities: Ember.computed('pokemon.abilityOne', 'pokemon.abilityTwo', function(){
     return this.get('pokemon.abilityTwo') && this.get('pokemon.abilityTwo') !== this.get('pokemon.abilityOne');
   }),
-  isExpanded: false,
-  isEditable: false,
   actions: {
-    toggleExpanded: function() {
-      this.toggleProperty('isExpanded');
-      if (!this.get('isExpanded')) {
-        this.set('isEditable', false);
-      }
-    },
-    toggleEditable: function() {
-      this.toggleProperty('isEditable');
-    },
     updatePokemon: function() {
       console.log('Component Action : updatePokemon');
       this.sendAction('routeUpdatePokemon');
